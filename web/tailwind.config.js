2bc0dec102123ddfa21668c5dca408c4205380e5
Sri Panyam <sri.panyam@gmail.com>
2026-03-24 10:40:46 -0700

    Fix Tailwind content paths to scan Go templates for class purging
    Content paths were '../templates/' (resolving to project root, wrong)
    instead of './templates/' (resolving to web/templates/, correct).
    This caused all Tailwind utility classes used in Go templates
    (grid, cards, badges, etc.) to be purged from the CSS bundle.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,8 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "../templates/**/*.html",
-    "../templates/*.html"
+    "./templates/**/*.html",
   ],
   darkMode: 'class',
   theme: {
